3e240db180e3a61f17d7ed316408fda16f4b6809
Nanao Suresh <suresh@globizs.com>
2025-09-01 18:43:31 +0000

    remove the auth checking for now in dashboard and /dashboard/new-applica

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,21 +4,12 @@ import {
   SidebarTrigger,
 } from '@/components/ui/sidebar';
 import { SidebarNav } from '@/components/layout/sidebar-nav';
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
-import { checkAuth } from '../actions';
 
 export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const isLoggedIn = await checkAuth();
-
-  if (!isLoggedIn) {
-    redirect('/');
-  }
-
   return (
     <SidebarProvider>
       <SidebarNav />
