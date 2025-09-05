31bd095f02b77e31671188cdc0ce9bf468aac56a
Nanao Suresh <suresh@globizs.com>
2025-09-05 07:57:08 +0000

    in the DLC Recommendations list on the forward button show this report d

--- src/app/dashboard/dlc-recommendations/page.tsx
+++ src/app/dashboard/dlc-recommendations/page.tsx
@@ -1,6 +1,6 @@
 
 import { DlcRecommendationsTable } from '@/components/applications/dlc-recommendations-table';
-import { getApplications } from '@/app/actions';
+import { getApplications, getApplicationStatuses } from '@/app/actions';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import { ServerLogHandler } from '@/components/debug/server-log-handler';
@@ -13,16 +13,23 @@ export default async function DlcRecommendationsPage() {
     redirect('/');
   }
 
-  const { data: initialApplicationsData, log } = await getApplications(accessToken);
+  const [{ data: initialApplicationsData, log: appLog }, { data: statuses, log: statusesLog }] = await Promise.all([
+    getApplications(accessToken),
+    getApplicationStatuses(accessToken)
+  ]);
 
   return (
     <>
-      <ServerLogHandler logs={[log]} />
+      <ServerLogHandler logs={[appLog, statusesLog]} />
       <div className="flex-1 space-y-4 px-4 md:px-8">
         <div className="flex items-center justify-between space-y-2">
           <h1 className="text-3xl font-bold tracking-tight font-headline">DLC Recommendations</h1>
         </div>
-        <DlcRecommendationsTable initialData={initialApplicationsData} accessToken={accessToken} />
+        <DlcRecommendationsTable 
+          initialData={initialApplicationsData} 
+          accessToken={accessToken} 
+          statuses={statuses}
+        />
       </div>
     </>
   );
